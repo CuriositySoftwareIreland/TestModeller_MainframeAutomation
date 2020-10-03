237ac6fec2f1dfa1ce4333f8c55237d1d6dcdebd
James Walker <james@testflip.co.uk>
2020-10-03 14:05:13 +0100

    Auto Commit

--- Framework/src/test/java/tests/TestModel_DefaultProfile.java
+++ Framework/src/test/java/tests/TestModel_DefaultProfile.java
@@ -10,15 +10,30 @@ import org.testng.annotations.Test;
 
 //http://mainframedemo.cloud.testinsights.io/app/#!/model-engine/guid/794855fa-a6d7-47bd-8768-c1d7775b1a52
 @Listeners({TestNGListener.class, ExtentTestListener.class})
-@TestModellerSuite(id = 3, profileId = 100001)
+@TestModellerSuite(id = 4, profileId = 100001)
 public class TestModel_DefaultProfile extends MainframeTestBase
 {
     
 
     
-    @Test  (groups= {"2567b035-63aa-44b0-901a-4a7de724ac2b","Test Model","Test Model - Default Profile"})
-    @TestModellerPath(guid = "2567b035-63aa-44b0-901a-4a7de724ac2b")
-    public void WaitForChangeEnterApplication()
+    @Test  (groups= {"7ad4b3e1-89c6-4165-9f06-2d07eaa8b500","Test Model","Test Model - Default Profile"})
+    @TestModellerPath(guid = "7ad4b3e1-89c6-4165-9f06-2d07eaa8b500")
+    public void WaitForChangeEnterApplicationGoToUrlPositiveEnterUsername1()
+    {
+        
+        pages.MainframeAutomation.MainframeCommands	_MainframeCommands = new pages.MainframeAutomation.MainframeCommands (session);
+    TestModellerLogger.SetLastNodeGuid("74ba49b2-a043-4f09-bdd9-4d74c7e0123d");
+    _MainframeCommands.WaitForChange(3000);
+
+pages.MainframeAutomation.MainApplication	_MainApplication = new pages.MainframeAutomation.MainApplication (session);
+    TestModellerLogger.SetLastNodeGuid("554ff9cc-8d07-4485-8e10-2e7b32f3046e");
+    _MainApplication.EnterApplication("daefts");
+
+    }
+
+    @Test  (groups= {"fb786d08-9023-4eff-bed6-55df36942f83","Test Model","Test Model - Default Profile"})
+    @TestModellerPath(guid = "fb786d08-9023-4eff-bed6-55df36942f83")
+    public void WaitForChangeEnterApplicationGoToUrlNegativeEnterUsername2()
     {
         
         pages.MainframeAutomation.MainframeCommands	_MainframeCommands = new pages.MainframeAutomation.MainframeCommands (session);
