d4f3756fd922cb146e1a62a1357cbd87ee735d59
TestFlip <james@Testflip.co.uk>
2022-02-06 14:54:41 +0000

    Add credentials

--- Framework/src/test/java/tests/MainframeTestBase.java
+++ Framework/src/test/java/tests/MainframeTestBase.java
@@ -6,6 +6,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import pages.MainframeAutomation.MainframeCommands;
 import utilities.PropertiesLoader;
 import utilities.Session;
 import utilities.reports.ExtentReportManager;
@@ -21,6 +22,7 @@ public class MainframeTestBase extends TestBase {
 		session.getProperties().set("jagacy.port", PropertiesLoader.getProperties().getProperty("mainframe.port"));
 
         session.open();
+        new MainframeCommands(session).WaitForChange(2000);
     }
 
     @AfterMethod(alwaysRun = true)
